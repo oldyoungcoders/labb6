3eea80b53e6c06584ccc439037a06424b0156bd6
oldyoungcoders <132730577+oldyoungcoders@users.noreply.github.com>
2023-11-01 19:45:51 +0800

    Add files via upload
    lab6

---
+++ app.js
@@ -0,0 +1,536 @@
+"use strict"; 
+var c = "";
+
+
+
+
+
+
+
+var too1 = +prompt("Bid chamaas durvun too asuuh bolno. Hariug console deer haraarai! Ehnii toog oruulna uu?");
+var too2 = +prompt("Daraagiin toog oruulna uu?");
+var too3 = +prompt("Gurvan dahi toog oruulna uu?");
+var too4 = +prompt("Suuliin toog oruulna uu?");
+
+if (too1<too2 && too1<too3 && too1<too4) {
+    console.log (too1 + " hamgiin baga too . Neg deh dasgal duusav.");
+} else if (too2<too1 && too2<too3 && too2<too4) {
+    console.log (too2 + " hamgiin baga too . Neg deh dasgal duusav.");
+} else if (too3<too1 && too3<too2 && too3<too4) {
+    console.log (too3 + " hamgiin baga too . Neg deh dasgal duusav.");
+} else if (too4<too1 && too4<too2 && too4<too3) {
+    console.log (too4 + " hamgiin baga too . Neg deh dasgal duusav.");
+} else if (too1==too2 && too3==too4 && too1==too3 && too2==too4) {
+    console.log ("Buh toonuud tencuu bna.  Neg deh dasgal duusav.");
+} 
+    
+
+
+console.log(c);
+
+
+
+var a= +prompt("Enehuu dasgald nai-aas deesh toonii niilberiig olno. Ehnii toog oruulna uu?");
+var b= +prompt("Daraagiin toog oruulna uu?");
+var c= +prompt("Gurvan dahi toog oruulna uu?");
+var d= +prompt("Suuliin toog oruulna uu?");
+
+if(a>=80){
+    a=a;
+} else {
+    a=0;
+}
+
+if(b>=80){
+    b=b;
+} else {
+    b=0;
+}
+
+if(c>=80){
+    c=c;
+} else {
+    c=0;
+}
+
+if(d>=80){
+    d=d;
+} else {
+    d=0;
+}
+
+console.log(a+b+c+d);
+
+
+
+console.log(c);
+
+
+
+var t1 = +prompt("7-aas baga 4 too oruul. Ehnii toog bich!");
+var t2 = +prompt("Daraagiin toog bich!");
+var t3 = +prompt("Gurvan dahi toog bich!");
+var t4 = +prompt("Suuliin toog bich!");
+
+if (t1<=5) {
+    t1=t1;
+} else {
+    t1=1;
+}
+
+if (t2<=5) {
+    t2=t2;
+} else {
+    t2=1;
+}
+
+if (t3<=5) {
+    t3=t3;
+} else {
+    t3=1;
+}
+
+if (t4<=5) {
+    t4=t4;
+} else {
+    t4=1;
+}
+
+console.log(t1 * t2 * t3 * t4 + " 2-r dasgal duuslaa.");
+
+
+
+console.log(c);
+
+
+
+var v1 = +prompt("Ugugdsun 3 toonoos aidaj neg ni tegsh baihaar bich ! Ehnii toog bich!");
+var v2 = +prompt("Daraagiin toog bich!");
+var v3 = +prompt("Gurvan dahi toog bich!");
+
+if (v1%2==0) {
+    v1=v1;
+} else {
+    v1=0;
+}
+
+if (v2%2==0) {
+    v2=v2;
+} else {
+    v2=0;
+}
+
+if (v3%2==0) {
+    v3=v3;
+} else {
+    v3=0;
+}
+
+console.log(v1 + v2 + v3 + " ,,, 3-r dasgal duuslaa.");
+
+
+console.log(c);
+
+
+
+
+var x1 = +prompt("Zuvhun sondgoi too bich! Ugui bol Javascript ajillahgui! Ehnii toog bich!");
+var x2 = +prompt("Daraagiin toog bich!");
+var x3 = +prompt("Gurvan dahi toog bich!");
+
+if (x1%3==0 || x1%5==0 || x1%7==0 || x1%9==0 ) {
+    x1=x1;
+} else {
+    x1=1;
+}
+
+if (x1%3==0 || x1%5==0 || x1%7==0 || x1%9==0 ) {
+    x2=x2;
+} else {
+    x2=1;
+}
+
+if (x1%3==0 || x1%5==0 || x1%7==0 || x1%9==0 ) {
+    x3=x3;
+} else {
+    x3=1;
+}
+
+console.log(x1 * x2 * x3 + " Chinii 3 toog urjiluud ingej garna , 4-r dasgal duuslaa.");
+
+
+
+
+console.log(c);
+
+
+var y1 = +prompt("Durtai toogoo bicheerei! Ehnii toog bich!");
+var y2 = +prompt("Daraagiin toog bich!");
+var y3 = +prompt("Gurvan dahi toog bich!");
+var y4 = +prompt("Suuliin toog bich!");
+
+if (y1%11==0 ) {
+    y1=0;
+} else {
+    y1=y1;
+}
+
+if (y2%11==0 ) {
+    y2=0;
+} else {
+    y2=y2; 
+}
+
+if (y3%11==0 ) {
+    y3=0;
+} else {
+    y3=y3;
+}
+
+if (y4%11==0) {
+    y4=0;
+} else {
+    y4=y4;
+}
+
+console.log(y1 + y2 + y3 + y4 + " , 5-r dasgal duuslaa.");
+
+
+
+console.log(c);
+
+
+
+var z1 = +prompt("Hussen toogoo bicheerei ? ");
+
+if (z1>10){
+    console.log("YES! 6-r dasgal duuslaa.");
+} else {
+    console.log("NO! 6-r dasgal duuslaa.");
+}
+
+
+console.log(c);
+
+
+
+
+var u1 = +prompt("Hussen toogoo bicheerei ? ");
+
+if (u1<5){
+    console.log("YES! 7-r dasgal duuslaa.");    
+} else {
+    console.log("NO! 7-r dasgal duuslaa.");
+}
+
+
+
+console.log(c);
+
+
+
+var h1 = +prompt("Hussen toogoo bicheerei ? ");
+var h2 = +prompt("Daraagiin toog bich!");
+var h3 = +prompt("Gurvan dahi toog bich!");
+
+if (h1%2==0){
+    console.log("YES! ");    
+} else {
+    console.log("NO! ");
+}
+
+if (h2%2==0){
+    console.log("YES! ");    
+} else {
+    console.log("NO! ");
+}
+
+if (h3%2==0){
+    console.log("YES! 8-r dasgal duuslaa.");    
+} else {
+    console.log("NO! 8-r dasgal duuslaa.");
+}
+
+
+
+console.log(c);
+
+
+
+var a1 = +prompt("Uldsen neg ni uu ch baij bolno. Toog oruul !!! ");
+var a2 = +prompt("Toog oruul !!!");
+var a3 = +prompt("Too oruul, gehdee zaaval ene gurvan toonii aidaj neg ni 5 baih oistoi !!!");
+
+
+if (a1 && a2){
+
+    console.log("YES! There is two 5 too in bodlogo. 9-r dasgal duuslaa.");    
+
+} else if (a1 && a3){
+
+    console.log("YES! There is two 5 too in bodlogo. 9-r dasgal duuslaa.");
+
+} else if (a2 && a3){
+
+    console.log("YES! There is two 5 too in bodlogo. 9-r dasgal duuslaa.");
+
+}
+
+
+
+if (a1 == 5 ){
+    a1=a1;
+
+    console.log("YES! There is one 5 too in bodlogo. 9-r dasgal duuslaa.");
+
+} else {
+    a1=a1;
+
+    console.log("NO! There is no 5 too in the a1 . 9-r dasgal duuslaa.");
+}
+
+
+
+if (a2 == 5 ){
+    a2=a2;
+
+    console.log("YES! There is one 5 too in bodlogo. 9-r dasgal duuslaa.");
+
+} else {
+    a2=a2;
+
+    console.log("NO! There is no 5 too in the a2 . 9-r dasgal duuslaa.");
+
+
+
+
+}
+
+if (a3 == 5 ){
+    a3=a3;
+    console.log("YES! There is one 5 too in bodlogo. 9-r dasgal duuslaa.");
+
+} else {
+    a3=a3;
+
+    console.log("NO! There is no 5 too in the a3 . 9-r dasgal duuslaa.");
+}
+
+
+
+console.log(c);
+
+
+
+
+var b1 = +prompt("Uldsen neg ni uu ch baij bolno. Toog oruul !!! ");
+var b2 = +prompt("Toog oruul !!!");
+var b3 = +prompt("Too oruul, gehdee zaaval ene gurvan toonii aidaj neg hoir 3-t huvaagddag baih oistoi !!!");
+
+
+if (b1 && b2){
+
+    console.log("YES! There is two 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");    
+
+} else if (b1 && b3){
+
+    console.log("YES! There is two 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");  
+
+} else if (b2 && b3){
+
+    console.log("YES! There is two 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");  
+
+}
+
+
+
+
+if (b1 % 3 == 0 ){
+    b1=b1;
+
+    console.log("YES! There is one 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");  
+
+} else {
+    b1=b1;
+
+    console.log("NO! There is no 3-t huvaagddag too in the a1 . 9-r dasgal duuslaa.");
+}
+
+
+
+if (b2 == 5 ){
+    b2=b2;
+
+    console.log("YES! There is one 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");  
+
+} else {
+    b2=b2;
+
+    console.log("NO! There is no 5 too in the a2 . 9-r dasgal duuslaa.");
+
+
+
+
+}
+
+if (b3 == 5 ){
+    b3=b3;
+
+    console.log("YES! There is one 3-t huvaagddag too in bodlogo. 10-r dasgal duuslaa.");  
+
+} else {
+    b3=b3;
+
+    console.log("NO! There is no 5 too in the a3 . 9-r dasgal duuslaa.");
+}
+
+
+
+console.log(c);
+
+
+
+var sar = +prompt("Sar oruulna uu? 1-12 hurtel bichne uu?");
+
+switch (sar) {
+    case 1:
+    case 12:
+    case 11:
+        console.log("Winter");
+        break;
+    case 2:
+    case 3:
+    case 4:
+        console.log("Spring");
+        break;
+    case 5:
+    case 6:
+    case 7:
+        console.log("Summer");
+        break;
+    case 8:
+    case 9:
+    case 10:
+        console.log("Autumn");
+        break;
+} 
+
+
+
+
+console.log(c);
+
+
+
+
+var garag = +prompt("Udur oruulna uu? 1-5 hurtel bichne uu?");
+
+switch (garag) {
+    case 1:
+        console.log("Davaa");
+        break;
+    case 2:
+        console.log("Maigmar");
+        break;
+    case 3:
+        console.log("Lhavga");
+        break;
+    case 4:
+        console.log("Purev");
+        break;
+    case 5:
+        console.log("Baasan");
+        break;
+} 
+
+
+
+console.log(c);
+
+
+
+
+
+var dun = +prompt("Chi shalgalt deeree hed avdag ve?");
+
+switch (true) {
+    case dun<59:
+        console.log("Davtdalgand aiv.'F' gedeg chin mash muu baina");
+        break;
+    case dun<69:
+        console.log("F-iig bodvol D dajgui ch bagsh chin chamd ih uurlana daa !");
+        break;
+    case dun<79:
+        console.log("Hmm ? C aig dundaj ch gesen hicheelee udur bolgon gertee davtaj baih shaardlagatai!!!");
+        break;
+    case dun<89:
+        console.log("Sain baina. Ulam sain hicheegeerei! B dajgiu shuu!");
+        break;
+    case dun<=100:
+        console.log("Mundag! Bayr hurgei . Uunsh gegee onc dun avch baigaarai! A ih sain.");
+        break;
+    default:
+        console.log("1-100 hurtel l toog oruulah oistoi!");
+        break;
+}
+
+
+
+
+
+console.log(c);
+
+
+
+
+
+
+var tooon = +prompt("Uuriigee unelj  1-5 hurtel too oruulna uu? ");
+
+switch (tooon) {
+    case 2:
+        console.log("Muu");
+        break;
+
+    case 3:
+        console.log("Dund");
+        break;
+
+    case 4:
+        console.log("Sain");
+        break;
+
+    case 5:
+        console.log("Onc");
+        break;
+
+} 
+
+
+
+
+
+console.log(c);
+
+
+
+
+
+
+
+
+var ten = +prompt("2-5 hurtel uuriigee unelnuu? 3 - aas deesh bol sain . ");
+
+switch (ten) {
+    case ten == 4,5:
+        console.log("Tentssen");
+        break;
+    case ten == 3,2,1:
+        console.log("Unasan");
+        break;
+
+}
+
+
+
+
